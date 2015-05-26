f45ec6b500e4840a51aafefa5cf09f7667e5f2d3
JN <naughton.joshua@gmail.com>
2015-05-26 05:32:51 -0400

    Initial commit

---
+++ BitcoinTalkSearch/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace BitcoinTalkSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cmdGo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.txtDays = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdGo
+            // 
+            this.cmdGo.Location = new System.Drawing.Point(261, 116);
+            this.cmdGo.Name = "cmdGo";
+            this.cmdGo.Size = new System.Drawing.Size(75, 23);
+            this.cmdGo.TabIndex = 0;
+            this.cmdGo.Text = "Go";
+            this.cmdGo.UseVisualStyleBackColor = true;
+            this.cmdGo.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 67);
+            this.label1.TabIndex = 1;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.Location = new System.Drawing.Point(69, 93);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(100, 20);
+            this.txtKeyword.TabIndex = 2;
+            this.txtKeyword.Text = "ANN";
+            // 
+            // txtDays
+            // 
+            this.txtDays.Location = new System.Drawing.Point(69, 119);
+            this.txtDays.Name = "txtDays";
+            this.txtDays.Size = new System.Drawing.Size(33, 20);
+            this.txtDays.TabIndex = 3;
+            this.txtDays.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Search:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Days:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 154);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDays);
+            this.Controls.Add(this.txtKeyword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdGo);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BitcoinTalk Auto Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdGo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.TextBox txtDays;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BitcoinTalkSearch/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+
+
+
+namespace BitcoinTalkSearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://bitcointalk.org/index.php?action=search;advanced");
+
+            String keyword = txtKeyword.Text;
+            String days = txtDays.Text;
+            String sortBy = "Most recent topics first";
+
+
+            try
+            {
+
+                //driver.FindElement(By.Name("search")).SendKeys(keyword);
+
+                IWebElement bodyArea = driver.FindElement(By.Id("bodyarea"));
+                bodyArea.FindElement(By.Name("search")).SendKeys(keyword);
+                bodyArea.FindElement(By.Id("subject_only")).Click();
+                bodyArea.FindElement(By.Name("maxage")).Clear();
+                bodyArea.FindElement(By.Name("maxage")).SendKeys(days);
+
+                IWebElement dropdown = bodyArea.FindElement(By.Name("sort"));
+                SelectElement dropClick = new SelectElement(dropdown);
+                dropClick.SelectByText(sortBy);
+
+                bodyArea.FindElement(By.Id("check_all")).Click();
+                bodyArea.FindElement(By.Id("exandBoardsIcon")).Click();
+                bodyArea.FindElement(By.Id("brd159")).Click();
+
+                bodyArea.FindElement(By.Name("submit")).Click();
+
+            } catch (Exception ex) {
+                MessageBox.Show("Selenium was unable to locate an element on the page.");
+            }
+
+        }
+
+    }
+}
